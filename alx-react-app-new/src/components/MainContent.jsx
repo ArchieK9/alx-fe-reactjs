e909c98bfab5019ae23e6415007d2cc7ad51de43
Achilike Justice <achilikejustice21@gmail.com>
2025-07-27 23:16:12 +0100

    Initial Commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent (){
+    return (
+      <main
+        style={{
+          padding: "20px",
+          backgroundColor: "#eef",
+          fontSize: "1.1em",
+        }}
+      >
+        <p>Explore some of the most beautiful cities around the world!</p>
+      </main>
+    );
+}
+
+export default MainContent;
