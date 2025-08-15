eb2419a15b47a32931729ff06f6d15645ee9e256
Achilike Justice <achilikejustice21@gmail.com>
2025-08-15 14:02:55 +0100

    Import component

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
 
   return (
     <>
-      <h1>
-        Hello World
-      </h1>
+    <UserProfile /> 
     </>
   )
 }
