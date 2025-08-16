69a2101d12565ec24522825270b58cc9154892b5
Achilike Justice <achilikejustice21@gmail.com>
2025-08-16 14:44:49 +0100

    Import react router

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,17 +1,24 @@
 import "./index.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
+    <Router>
     <div className="bg-blue-100 min-h-screen w-screen mx-auto">
       <header className="bg-blue-600 text-white p-4 text-center text-xl font-bold">
         Recipe Sharing Platform
       </header>
-      <HomePage />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </main>
     </div>
+    </Router>
   );
 }
 
 export default App;
-
